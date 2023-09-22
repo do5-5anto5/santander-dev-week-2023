4c986f1ba4bf5ed288ab02c10f9856e74eb8541e
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-09-22 17:28:42 -0300

    Domain layer implementation

---
+++ src/main/java/santanderapi/domain/model/Account.java
@@ -0,0 +1,65 @@
+package santanderapi.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_account")
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+    String number;
+	
+    String agency;
+    
+    @Column(scale = 13, precision = 2)
+    BigDecimal balance;
+    @Column(name = "addtional_limit", scale = 13, precision = 2)
+    BigDecimal limit;
+
+    public Account(String number, String agency) {
+        this.number = number;
+        this.agency = agency;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
